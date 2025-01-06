53a5536c6d41c3f2f0216c8dbc94b27d0e01dba0
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-06 20:30:08 +0545

    added app for user account

---
+++ School/UserProfile/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class UserProfile(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    user_password = models.CharField(max_length=100)
+    user_phone = models.CharField(max_length=10)
+    user_address = models.TextField()
+    user_dob = models.DateField()
+    user_profile_pic = models.ImageField(upload_to='UserProfile/ProfilePic/')
+
+    def __str__(self):
+        return self.user_name
+
+    class Meta:
+        verbose_name_plural = 'User Account'
