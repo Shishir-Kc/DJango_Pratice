615776acc56bfb06c2e3949f9a56e52c404cda4f
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-13 10:33:18 +0545

    it is uselesss i will be removing this pice

--- School/Home/forms.py
+++ School/Home/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class StudentFrom(forms.Form):
-    user_name = forms.CharField(max_length=100)
+    user_name = forms.CharField(max_length=100 , label="Student Name = > ")
+    user_pass = forms.CharField(widget=forms.PasswordInput())
