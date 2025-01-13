4dadf7a5f86f423121e01505059fff5e54cb472e
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-13 10:35:43 +0545

    change from class based to function based view !

--- School/Home/urls.py
+++ School/Home/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('notice/<int:pk>',views.NoticeDetail.as_view(),name='notice_detail'),
     path('notice/delete/<int:pk>',views.NoticeDelete.as_view(),name='notice_delete'),
     path('notice/add/',views.NoticeAdd.as_view(),name='notice_add'),
-    path('login/',views.USER_LOGIN.as_view(),name='login'),
+    path('login/',views.USER_LOGIN,name='login'),
     path('test/',views.AddUser,name='test'),
     
 ]
