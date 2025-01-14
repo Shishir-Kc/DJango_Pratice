490976c4a327d3faefc11e1fdb7a3af2a59a8c7d
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-14 13:37:39 +0545

    updated to function based

--- School/Home/urls.py
+++ School/Home/urls.py
@@ -5,12 +5,12 @@ from django.views.generic import TemplateView
 app_name ="Home"
 
 urlpatterns = [ 
-    path('',views.Notice.as_view(),name='home'),  
-    path('notice/',views.Notice.as_view(),name='notice'),
+    path('notice/',views.Home,name='home'),
     path('notice/<int:pk>',views.NoticeDetail.as_view(),name='notice_detail'),
     path('notice/delete/<int:pk>',views.NoticeDelete.as_view(),name='notice_delete'),
-    path('notice/add/',views.NoticeAdd.as_view(),name='notice_add'),
+    path('notice/add/',views.NoticeAdd,name='notice_add'),
     path('login/',views.USER_LOGIN,name='login'),
     path('test/',views.AddUser,name='test'),
+    path("logout/",views.user_logout,name='logout'),
     
 ]
