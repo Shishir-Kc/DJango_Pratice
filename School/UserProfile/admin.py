6b92f7f2b6b89c4f6ea6e00454cae6db5ead0c9f
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-14 13:38:45 +0545

    fixed some bugs

--- School/UserProfile/admin.py
+++ School/UserProfile/admin.py
@@ -4,5 +4,5 @@ from . import models
 
 admin.site.register(models.UserAccount)
 admin.site.register(models.UserProfile)
-
+admin.site.register(models.Admin)
 admin.site.register(models.School_class)
