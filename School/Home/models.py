4ff3a2304be5f9161cae9d8648c05f048a0ffda3
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-14 13:34:55 +0545

    added user model

--- School/Home/models.py
+++ School/Home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Notice(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     date_uploaded = models.DateTimeField(auto_now_add=True)
     Topic = models.CharField(max_length=100)
     News = models.TextField()
